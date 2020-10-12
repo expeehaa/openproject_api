f3c5d6f065420a9e19482add6e1fe1b7edee46a9
expeehaa <expeehaa@outlook.com>
2020-10-12 20:25:40 +0200

    Include module OpenprojectApi::Client::WorkPackages in OpenprojectApi::Client.

--- lib/openproject_api/client.rb
+++ lib/openproject_api/client.rb
@@ -2,6 +2,10 @@ require_relative 'api'
 
 module OpenprojectApi
 	class Client < Api
-		
+		Dir[File.expand_path('client/*.rb', __dir__)].each do |f|
+			require f
+		end
+
+		include WorkPackages
 	end
 end
