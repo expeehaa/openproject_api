3460a8450c2843d935832c0e11d246b6061cb85d
expeehaa <expeehaa@outlook.com>
2020-10-12 20:10:39 +0200

    Add method OpenprojectApi::Client#request.

--- lib/openproject_api/client.rb
+++ lib/openproject_api/client.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'net/https'
+
 module OpenprojectApi
 	class Client
 		attr_accessor :endpoint
@@ -6,6 +9,25 @@ module OpenprojectApi
 		def initialize(endpoint:, apikey:)
 			self.endpoint = endpoint
 			self.apikey   = apikey
+		end
+		
+		def request(resource, query: {}, &block)
+			uri = URI.join(endpoint, resource)
+			uri.query = URI.encode_www_form(query)
+			
+			response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme=='https') do |http|
+				request = yield(uri)
+				
+				request.basic_auth('apikey', apikey)
+				
+				http.request(request)
+			end
+			
+			if response.is_a?(Net::HTTPOK)
+				JSON.parse(response.body)
+			else
+				response.error!
+			end
 		end
 	end
 end
