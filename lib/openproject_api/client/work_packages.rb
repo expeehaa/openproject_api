826ed293f8d18bb7b94b921ccb2586cb4585770f
expeehaa <expeehaa@outlook.com>
2020-10-12 20:47:26 +0200

    Add workpackage relations requests.

--- lib/openproject_api/client/work_packages.rb
+++ lib/openproject_api/client/work_packages.rb
@@ -16,6 +16,18 @@ module OpenprojectApi
 			def create_work_package(body, *args)
 				post('/api/v3/work_packages', body, *args)
 			end
+			
+			def work_package_relations(work_package_id, *args)
+				get("/api/v3/work_packages/#{work_package_id}/relations", *args)
+			end
+			
+			def add_work_package_relation(work_package_id, body, *args)
+				post("/api/v3/work_packages/#{work_package_id}/relations", body, *args)
+			end
+			
+			def work_package_available_relation_candidates(work_package_id, *args)
+				get("/api/v3/work_packages/#{work_package_id}/available_relation_candidates", *args)
+			end
 		end
 	end
 end
