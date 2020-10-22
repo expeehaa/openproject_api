c144b52a60d1e459850d3ca8b1484bc15f5b4189
expeehaa <expeehaa@outlook.com>
2020-10-23 01:11:03 +0200

    Add users requests.

---
+++ lib/openproject_api/client/users.rb
@@ -0,0 +1,33 @@
+module OpenprojectApi
+	class Client
+		module Users
+			def users(*args)
+				get('/api/v3/users', *args)
+			end
+			
+			def user(user_id, *args)
+				get("/api/v3/users/#{user_id}", *args)
+			end
+			
+			def patch_user(user_id, body, *args)
+				patch("/api/v3/users/#{user_id}", body, *args)
+			end
+			
+			def create_user(body, *args)
+				post("/api/v3/users", body, *args)
+			end
+			
+			def delete_user(user_id, *args)
+				delete("/api/v3/users/#{user_id}", *args)
+			end
+			
+			def lock_user(user_id, *args)
+				post("/api/v3/users/#{user_id}/lock", *args)
+			end
+			
+			def unlock_user(user_id, *args)
+				delete("/api/v3/users/#{user_id}/lock", *args)
+			end
+		end
+	end
+end
