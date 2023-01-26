044f314f3f0995f6153e828da98001d6db201288
expeehaa <expeehaa@outlook.com>
2023-01-26 15:15:26 +0100

    Move API methods into OpenprojectApi::Client.

--- lib/openproject_api/client.rb
+++ lib/openproject_api/client.rb
@@ -2,18 +2,200 @@ require 'openproject_api/api'
 
 module OpenprojectApi
 	class Client < Api
-		Dir[File.expand_path('client/*.rb', __dir__)].sort.each do |f|
-			require f
+		def projects(*args)
+			get('/api/v3/projects', *args)
 		end
 		
-		include Projects
-		include Relations
-		include Root
-		include Statuses
-		include TimeEntries
-		include Types
-		include UserPreferences
-		include Users
-		include WorkPackages
+		def project(project_id, *args)
+			get("/api/v3/projects/#{project_id}", *args)
+		end
+		
+		def update_project(project_id, body, *args)
+			patch("/api/v3/projects/#{project_id}", body, *args)
+		end
+		
+		def create_project(body, *args)
+			get('/api/v3/projects', body, *args)
+		end
+		
+		def delete_project(project_id, *args)
+			delete("/api/v3/projects/#{project_id}", *args)
+		end
+		
+		def project_types(project_id, *args)
+			get("/api/v3/projects/#{project_id}/types", *args)
+		end
+		
+		def project_available_parent_project_candidates(*args)
+			get('/api/v3/projects/available_parent_projects', *args)
+		end
+		
+		
+		def relations(*args)
+			get('/api/v3/relations', *args)
+		end
+		
+		def relation(relation_id, *args)
+			get("/api/v3/relations/#{relation_id}", *args)
+		end
+		
+		def update_relation(relation_id, body, *args)
+			patch("/api/v3/relations/#{relation_id}", body, *args)
+		end
+		
+		def delete_relation(relation_id, *args)
+			delete("/api/v3/relations/#{relation_id}", *args)
+		end
+		
+		
+		def root(*args)
+			get('/api/v3', *args)
+		end
+		
+		
+		def statuses(*args)
+			get('/api/v3/statuses', *args)
+		end
+		
+		def status(status_id, *args)
+			get("/api/v3/statuses/#{status_id}", *args)
+		end
+		
+		
+		def time_entries(*args)
+			get('/api/v3/time_entries', *args)
+		end
+		
+		def time_entry(time_entry_id, *args)
+			get("/api/v3/time_entries/#{time_entry_id}", *args)
+		end
+		
+		def update_time_entry(time_entry_id, body, *args)
+			patch("/api/v3/time_entries/#{time_entry_id}", body, *args)
+		end
+		
+		def create_time_entry(body, *args)
+			post('/api/v3/time_entries', body, *args)
+		end
+		
+		def delete_time_entry(time_entry_id, *args)
+			delete("/api/v3/time_entries/#{time_entry_id}", *args)
+		end
+		
+		def time_entries_available_projects(*args)
+			get('/api/v3/time_entries/available_projects', *args)
+		end
+		
+		
+		def types(*args)
+			get('/api/v3/types', *args)
+		end
+		
+		def type(type_id, *args)
+			get("/api/v3/types/#{type_id}", *args)
+		end
+		
+		
+		def my_preferences(*args)
+			get('/api/v3/my_preferences', *args)
+		end
+		
+		def update_my_preferences(body, *args)
+			patch('/api/v3/my_preferences', body, *args)
+		end
+		
+		
+		def users(*args)
+			get('/api/v3/users', *args)
+		end
+		
+		def user(user_id, *args)
+			get("/api/v3/users/#{user_id}", *args)
+		end
+		
+		def update_user(user_id, body, *args)
+			patch("/api/v3/users/#{user_id}", body, *args)
+		end
+		
+		def create_user(body, *args)
+			post('/api/v3/users', body, *args)
+		end
+		
+		def delete_user(user_id, *args)
+			delete("/api/v3/users/#{user_id}", *args)
+		end
+		
+		def lock_user(user_id, *args)
+			post("/api/v3/users/#{user_id}/lock", *args)
+		end
+		
+		def unlock_user(user_id, *args)
+			delete("/api/v3/users/#{user_id}/lock", *args)
+		end
+		
+		
+		def work_packages(*args)
+			get('/api/v3/work_packages', *args)
+		end
+		
+		def work_package(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}", *args)
+		end
+		
+		def update_work_package(work_package_id, body, *args)
+			patch("/api/v3/work_packages/#{work_package_id}", body, *args)
+		end
+		
+		def create_work_package(body, *args)
+			post('/api/v3/work_packages', body, *args)
+		end
+		
+		def delete_work_package(work_package_id, *args)
+			delete("/api/v3/work_packages/#{work_package_id}", *args)
+		end
+		
+		def work_package_relations(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/relations", *args)
+		end
+		
+		def add_work_package_relation(work_package_id, body, *args)
+			post("/api/v3/work_packages/#{work_package_id}/relations", body, *args)
+		end
+		
+		def work_package_available_relation_candidates(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/available_relation_candidates", *args)
+		end
+		
+		def work_package_watchers(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/watchers", *args)
+		end
+		
+		def add_work_package_watcher(work_package_id, body, *args)
+			post("/api/v3/work_packages/#{work_package_id}/watchers", body, *args)
+		end
+		
+		def delete_work_package_watcher(work_package_id, watcher_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/watchers/#{watcher_id}", *args)
+		end
+		
+		def work_package_available_watchers(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/available_watchers", *args)
+		end
+		
+		def work_package_activities(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/activities", *args)
+		end
+		
+		def add_work_package_activity(work_package_id, body, *args)
+			post("/api/v3/work_packages/#{work_package_id}/activities", body, *args)
+		end
+		
+		def work_package_available_assignees(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/available_assignees", *args)
+		end
+		
+		def work_package_available_responsibles(work_package_id, *args)
+			get("/api/v3/work_packages/#{work_package_id}/available_responsibles", *args)
+		end
 	end
 end
--- lib/openproject_api/client/root.rb
+++
@@ -1,9 +0,0 @@
-module OpenprojectApi
-	class Client
-		module Root
-			def root(*args)
-				get('/api/v3', *args)
-			end
-		end
-	end
-end
